4d5eb87e7f5969148e3d51db8a33a221143bfdb7
tejaswi27pixel <tejaswi.vta@gmail.com>
2024-08-20 19:48:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const fs = require("fs");
+const app = express();
+const cors = require("cors");
+
+var corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://web-cipherpad.abierta.cloud",
+    "https://web-cipherpad.abierta.cloud",
+    "https://www.web-cipherpad.abierta.cloud",
+    "http://192.168.1.3:3000",
+  ],
+  optionsSuccessStatus: 200,
+};
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
+
+const upload = multer(); // Use multer without specifying a destination
+
+const fileStore = {}; // In-memory storage for file metadata
+
+app.use(express.json());
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  const { description, password } = req.body;
+
+  if (!file) {
+    return res.status(400).json({ error: "File upload failed." });
+  }
+
+  const fileId = crypto.randomBytes(16).toString("hex");
+  const fileLink = `http://localhost:5000/file/${fileId}/download`;
+
+  // Store file metadata including the file buffer
+  fileStore[fileId] = {
+    fileBuffer: file.buffer, // Store the file buffer
+    description,
+    password,
+    originalName: file.originalname,
+  };
+
+  res.json({ fileLink });
+});
+
+app.get("/file/:fileId/download", (req, res) => {
+  const { fileId } = req.params;
+  const { password } = req.query;
+
+  const fileMetadata = fileStore[fileId];
+
+  if (!fileMetadata) {
+    return res.status(404).send("File not found.");
+  }
+
+  if (fileMetadata.password && fileMetadata.password !== password) {
+    return res.status(403).send("Incorrect password.");
+  }
+
+  // Send the file buffer directly
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename=${fileMetadata.originalName}`
+  );
+  res.setHeader("Content-Type", "application/octet-stream");
+  res.send(fileMetadata.fileBuffer);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
